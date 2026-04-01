c6fcfa27f6588e260364913d7ba8c951703994e4
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2026-04-01 19:04:46 -0400

    docs: finalize icanclean citations and resolve sphinx warnings

--- mne_denoise/icanclean/__init__.py
+++ mne_denoise/icanclean/__init__.py
@@ -19,6 +19,12 @@ References
 .. [3] Gonsisko, C. B., Ferris, D. P., & Downey, R. J. (2023). iCanClean
        Improves ICA of Mobile Brain Imaging with EEG. Sensors, 23(2), 928.
        https://doi.org/10.3390/s23020928
+.. [4] Nordin, A. D., Hairston, W. D., & Ferris, D. P. (2018). Dual-electrode
+       motion artifact cancellation for mobile electroencephalography.
+       Journal of Neural Engineering, 15(5), 056024.
+       https://doi.org/10.1088/1741-2552/aad7d7
+.. [5] Hotelling, H. (1936). Relations between two sets of variates.
+       Biometrika, 28(3/4), 321-377.
 """
 
 from .core import ICanClean, compute_icanclean
